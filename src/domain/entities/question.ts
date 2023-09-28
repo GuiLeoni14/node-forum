3ce747ec9494931fa4212d76970b91d536e318cd
GuiLeoni14 <guileone167@gmail.com>
2023-09-28 14:21:49 -0300

    feat: add lint

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,9 +1,9 @@
-import { randomUUID } from "node:crypto";
-import { Slug } from "./value-objects/slug";
-import { Entity } from "@/core/entities/entity";
-import { UniqueEntityID } from "@/core/entities/unique-entity-id";
-import { Optional } from "@/core/types/optional";
-import dayjs from "dayjs";
+import { randomUUID } from 'node:crypto'
+import { Slug } from './value-objects/slug'
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Optional } from '@/core/types/optional'
+import dayjs from 'dayjs'
 
 interface QuestionProps {
   authorId: UniqueEntityID
@@ -14,32 +14,32 @@ interface QuestionProps {
   createdAt: Date
   updatedAt?: Date
 }
-export class Question extends Entity<QuestionProps>{
-  get authorId(){
+export class Question extends Entity<QuestionProps> {
+  get authorId() {
     return this.props.authorId
   }
-  
-  get bestAnswerId(){
+
+  get bestAnswerId() {
     return this.props.bestAnswerId
   }
 
-  get title(){
+  get title() {
     return this.props.title
   }
-  
-  get content(){
+
+  get content() {
     return this.props.content
   }
 
-  get slug(){
+  get slug() {
     return this.props.slug
   }
-  
-  get createdAt(){
+
+  get createdAt() {
     return this.props.createdAt
   }
 
-  get updatedAt(){
+  get updatedAt() {
     return this.props.updatedAt
   }
 
@@ -47,37 +47,43 @@ export class Question extends Entity<QuestionProps>{
     return dayjs().diff(this.createdAt, 'days') <= 3
   }
 
-  get excerpt(){
+  get excerpt() {
     return this.content.substring(0, 120).trimEnd().concat('...')
   }
 
-  private touch(){
+  private touch() {
     this.props.updatedAt = new Date()
   }
 
-  set title(title: string){
+  set title(title: string) {
     this.props.title = title
     this.props.slug = Slug.createFromText(title)
 
     this.touch()
   }
 
-  set content(content: string){
+  set content(content: string) {
     this.props.content = content
     this.touch()
   }
 
-  set bestAnswerId(bestAnswerId: UniqueEntityID | undefined){
+  set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
     this.props.bestAnswerId = bestAnswerId
     this.touch()
   }
 
-  static create(props: Optional<QuestionProps, 'createdAt' | 'slug'>, id?: UniqueEntityID){
-    const question = new Question({
-      ...props,
-      slug: props.slug ?? Slug.createFromText(props.title),
-      createdAt: new Date()
-    }, id)
+  static create(
+    props: Optional<QuestionProps, 'createdAt' | 'slug'>,
+    id?: UniqueEntityID,
+  ) {
+    const question = new Question(
+      {
+        ...props,
+        slug: props.slug ?? Slug.createFromText(props.title),
+        createdAt: new Date(),
+      },
+      id,
+    )
 
     return question
   }
--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -3,18 +3,16 @@ import { AnswersRepository } from '../repositories/answer-repository'
 import { Answer } from '../entities/answer'
 
 const fakeAnswersRepository: AnswersRepository = {
-   create: async function (answer: Answer): Promise<void> {
-    return
-  }
+  create: async function (answer: Answer): Promise<void> {},
 }
 
-it('should be create an answer',async  () => {
+it('should be create an answer', async () => {
   const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
 
   const answer = await answerQuestion.execute({
     questionId: '1',
     instructorId: '1',
-    content: 'Nova resposta'
+    content: 'Nova resposta',
   })
 
   expect(answer.content).toEqual('Nova resposta')
