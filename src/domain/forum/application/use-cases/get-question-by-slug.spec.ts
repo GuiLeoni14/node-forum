0249b93a55297d7aeea53356cd3dc00ead325d92
GuiLeoni14 <guileone167@gmail.com>
2023-09-29 23:00:25 -0300

    feat: refactor tests and use-cases using either

--- src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -1,27 +1,31 @@
-import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
 import { GetQuestionBySlugUseCase } from './get-question-by-slug'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
 import { makeQuestion } from 'test/factories/make-question'
-import { Slug } from '../../enterprise/entities/value-objects/slug'
 
-let questionsRepository: InMemoryQuestionsRepository
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
 let sut: GetQuestionBySlugUseCase
 
-describe('Get question use case', () => {
+describe('Get Question By Slug', () => {
   beforeEach(() => {
-    questionsRepository = new InMemoryQuestionsRepository()
-    sut = new GetQuestionBySlugUseCase(questionsRepository)
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new GetQuestionBySlugUseCase(inMemoryQuestionsRepository)
   })
-  it('should be to get a question by slug', async () => {
+
+  it('should be able to get a question by slug', async () => {
     const newQuestion = makeQuestion({
       slug: Slug.create('example-question'),
     })
 
-    await questionsRepository.create(newQuestion)
+    await inMemoryQuestionsRepository.create(newQuestion)
 
-    const { question } = await sut.execute({
+    const result = await sut.execute({
       slug: 'example-question',
     })
 
-    expect(question.slug.value).toEqual(newQuestion.slug.value)
+    expect(result.isRight()).toBeTruthy()
+    expect(inMemoryQuestionsRepository.items[0]?.title).toEqual(
+      newQuestion.title,
+    )
   })
 })
