c785632477cfc3b874650d4e6749108a56d5ad7b
GuiLeoni14 <guileone167@gmail.com>
2023-09-30 00:58:00 -0300

    feat: delete many question attachment repositories

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,9 +1,14 @@
-import { Question } from '@/domain/forum/enterprise/entities/question'
-import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 import { PaginationParams } from '@/core/repositories/pagination-params'
+import { QuestionAttachmentsRepository } from '@/domain/forum/application/repositories/question-attachments-repository'
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
+import { Question } from '@/domain/forum/enterprise/entities/question'
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = []
+
+  constructor(
+    private questionAttachmentsRepository: QuestionAttachmentsRepository,
+  ) {}
 
   async findById(id: string) {
     const question = this.items.find((item) => item.id.toString() === id)
@@ -16,7 +21,21 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
   }
 
   async findBySlug(slug: string) {
-    return this.items.find((item) => item.slug.value === slug) ?? null
+    const question = this.items.find((item) => item.slug.value === slug)
+
+    if (!question) {
+      return null
+    }
+
+    return question
+  }
+
+  async findManyRecent({ page }: PaginationParams) {
+    const questions = this.items
+      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+      .slice((page - 1) * 20, page * 20)
+
+    return questions
   }
 
   async create(question: Question) {
@@ -29,17 +48,13 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     this.items[itemIndex] = question
   }
 
-  async findManyRecent({ page }: PaginationParams) {
-    const questions = this.items
-      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
-      .slice((page - 1) * 20, page * 20)
-
-    return questions
-  }
-
   async delete(question: Question) {
     const itemIndex = this.items.findIndex((item) => item.id === question.id)
 
     this.items.splice(itemIndex, 1)
+
+    this.questionAttachmentsRepository.deleteManyByQuestionId(
+      question.id.toString(),
+    )
   }
 }
